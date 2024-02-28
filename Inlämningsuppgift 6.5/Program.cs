976d231f4d3680c72e46d460235d94bdb2ce8547
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-02-28 10:52:55 +0100

    Add project files.

---
+++ Inlämningsuppgift 6.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uppgift_6._5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Längst("hej", "hallå");
+        }
+        static void Längst(string ord1, string ord2)
+        {
+            int Längd1 = ord1.Length;
+            int Längd2 = ord2.Length;
+
+            if(Längd1 > Längd2)
+            {
+                Console.WriteLine(ord1);
+            }
+            else if (Längd1 < Längd2)
+            {
+                Console.WriteLine(ord2);
+            }
+            else if (Längd1 == Längd2)
+            {
+                Console.WriteLine(ord1);
+            }
+        }
+    }
+}
